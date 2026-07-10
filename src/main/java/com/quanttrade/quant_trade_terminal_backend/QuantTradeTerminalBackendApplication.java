0669d89fcac5c2ca6417e144d95d51548d88978c
j2nooh <wisestorm01@gmail.com>
2026-07-10 09:55:15 +0900

    Initial backend project setup

---
+++ src/main/java/com/quanttrade/quant_trade_terminal_backend/QuantTradeTerminalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quanttrade.quant_trade_terminal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantTradeTerminalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantTradeTerminalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quanttrade/quant_trade_terminal_backend/QuantTradeTerminalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanttrade.quant_trade_terminal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantTradeTerminalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
